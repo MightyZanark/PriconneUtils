7b600c22949164b0e3e9a9448fe69a00c2c367a4
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2022-06-15 22:10:35 +0900

    Changed how DBCheck works and added L2D script

--- DBCheck.py
+++ DBCheck.py
@@ -3,49 +3,94 @@ import os, requests, json
 cur_dir = os.getcwd()
 max_test = 20
 test_multi = 10
-json_file = r'.\latest_ver.json'
+conf = r'.\config.json'
+db_dir = r'.\database'
+# manifest_iter = {'db': 'master.db', 'movie': 'moviemanifest', 'sound': 'soundmanifest'}
 
 def update_db():
+    #Checks if database folder exist, if it doesn't then create one
+    if not os.path.isdir(db_dir):
+        os.mkdir(db_dir)
+
     print('Checking for database updates...')
-    with open(json_file, 'r+') as j:
-        latest = json.load(j)
-        version = int(latest["TruthVersion"])
+    with open(conf, 'r+') as j:
+        c = json.load(j)
+        version = int(c["TruthVersion"])
+        assetmanifest = str(c["assetmanifest"])
         i = 1
+
+        #While loop to check for new versions
         while i <= max_test:
             guess = version + (i * test_multi)
-            r = requests.get(f'http://prd-priconne-redive.akamaized.net/dl/Resources/{guess}/Jpn/AssetBundles/Windows/manifest/manifest_assetmanifest')
+            r = requests.get(assetmanifest.replace('version', str(guess)))
             if r.status_code == 200:
                 print(f'[{guess}] is a valid new version. Checking for more...')
                 version = guess
                 i = 1
             else:
+                # print(f'[{guess}] is not valid')
                 i += 1
         
-        r = requests.get(f'http://prd-priconne-redive.akamaized.net/dl/Resources/{version}/Jpn/AssetBundles/Windows/manifest/masterdata_assetmanifest').content
-        l = str(r).split(',')
-        hash = l[1]
-        
-        if version == int(latest["TruthVersion"]):
-            if not os.path.isfile('master.db'):
-                print(f'No database file detected, downloading the latest: [{version}]')
-                download_db(hash)
-                print("> Finished downloading the latest database!")
-            else:
-                print("> Database is up to date!")
+        #Checks if the TruthVersion is the same as in the latest_ver.json
+        if version == int(c["TruthVersion"]):
+            
+            #Checks if master.db or manifests file exist. If it doesn't, then download them
+            #If anyone has a better way to approach this, you can do a pull request or make a suggestion from the "Issues" tab
+            while True:
+                if not os.path.isfile(f'{db_dir}\\master.db'):
+                    print(f'No database file detected, downloading the latest: [{version}]')
+                    download_manifest('db')
+                elif not os.path.isfile(f'{db_dir}\\moviemanifest'):
+                    print("No moviemanifest file detected, downloading...")
+                    download_manifest('movie')
+                elif not os.path.isfile(f'{db_dir}\\sound2manifest'):
+                    print("No sound2manifest file detected, downloading...")
+                    download_manifest('sound')
+                else:
+                    print("> Database is up to date!")
+                    break
+
         else:
-            print(f'New version available: [{version}], updating database...')
-            download_db(hash)
-            latest = {"TruthVersion": version, "hash": hash}
+            #Updates config.json
+            r = requests.get(str(c["masterdata"]).replace('version', str(version))).content
+            l = str(r).split(',')
+            hash = l[1]
+            c["TruthVersion"] = version
+            c["hash"] = hash
             j.seek(0)
-            json.dump(latest, j)
+            json.dump(c, j, indent=1)
+
+            #Downloads the new database alongside manifests
+            print(f'New version available: [{version}], updating database...')
+            download_manifest('db')
+            download_manifest('sound')
+            download_manifest('movie')
             print("> Update completed!")
+
+#Downloads .db and sound/movie manifest to use for L2D, BGM extraction, etc.
+def download_manifest(type):
+    with open(conf, 'r') as f:
+        data = json.load(f)
+        t = str(data[type])
+        hash = data["hash"]
+        manifest_name = t.split("/")[-1]
+        ver = str(data["TruthVersion"])
         
-def download_db(hash):
-    database = requests.get(f'http://prd-priconne-redive.akamaized.net/dl/pool/AssetBundles/{hash[:2]}/{hash}').content
-    with open('master.cdb', 'wb') as cdb:
-        cdb.write(database)
-    os.system(f'Coneshell_call.exe -cdb master.cdb master.db')
-    os.remove('master.cdb')
+        if type == 'db':
+            print("Downloading database...")
+            database = requests.get(f'{t}/{hash[:2]}/{hash}').content
+            with open('master.cdb', 'wb') as cdb:
+                cdb.write(database)
+            os.system(f'Coneshell_call.exe -cdb master.cdb "{db_dir}\\master.db"')
+            os.remove('master.cdb')
+            print("> Finished downloading database!\n")
+
+        else:
+            print(f'Downloading {manifest_name}...')
+            manifest = requests.get(t.replace('version',ver)).content
+            with open(f'{db_dir}\\{manifest_name}', 'wb') as m:
+                m.write(manifest)
+            print("> Download completed!\n")
 
 if __name__ == '__main__':
     update_db()
--- README.md
+++ README.md
@@ -4,13 +4,17 @@ Scripts to get the master.db, BGM files, L2D, UB Cutin, etc. from the game Princ
 
 ## Requirements
 
+- Internet Connection (obviously)
 - [Python 3.x](https://www.python.org/downloads/)
 - Requests library for Python `pip install requests`
-- [UsmToolkit](https://github.com/MightyZanark/UsmToolkit)
-- Internet Connection
+- [UsmToolkit (for video files)](https://github.com/MightyZanark/UsmToolkit)
+- [FFmpeg (for audio files and UsmToolkit)](https://ffmpeg.org/download.html)
 - [.NET 3.1 Core Runtime (for UsmToolkit)](https://dotnet.microsoft.com/en-us/download/dotnet/3.1/runtime)
+
+Note: If you don't already have FFmpeg installed, then you can just do it from UsmToolkit. UsmToolkit have a command to download FFmpeg.
 
 ## How to Use
 
-1. Well first of all, install all the requirements, won't work without.
-2. 
+1. Well first of all, install all the requirements, won't work without it.
+2. If you haven't already, add Python, UsmToolkit, and FFmpeg to PATH in System Variables. If you don't know how to do that then check out the later section. Additionally, if you got FFmpeg from UsmToolkit, then as long as UsmToolkit is in PATH, you are good to go.
+3. 
